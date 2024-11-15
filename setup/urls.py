a1f6f431a3cb2a5969620a596e4e87f6cd7eb5b4
wendellsantos2 <wemdellcos@gmail.com>
2024-11-15 14:46:52 -0300

    feat: selecao de fotos disponivel

--- setup/urls.py
+++ setup/urls.py
@@ -4,5 +4,6 @@ from galeria.views import index,imagem
 
 urlpatterns = [
         path('', index, name='index'),
+        path('admin/', admin.site.urls),
         path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
